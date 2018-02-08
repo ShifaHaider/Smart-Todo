535a894d3519c1a8cde407c213c846b0af8a6f11
Shifa Haider <shifa.haider065@gmail.com>
2018-02-08 15:00:47 +0500

    open login page

--- src/routes/register/register.js
+++ src/routes/register/register.js
@@ -1,5 +1,5 @@
 var db = firebase.firestore();
-function saveData(){
+function saveData() {
     var name = document.getElementById('name');
     var userName = document.getElementById('userName');
     var email = document.getElementById('email');
@@ -15,18 +15,20 @@ function saveData(){
 
     var ref = db.collection('users').doc(saveData1.userName);
 
-    ref.get().then(function(doc){
-        if(doc.exists){
+    ref.get().then(function (doc) {
+        if (doc.exists) {
             alert('UserName is not available');
         }
-        else{
-            ref.set(saveData1);
-            name.value = '';
-            userName.value = '';
-            email.value = '';
-            password.value = '';
-            phone.value = '';
+        else {
+            ref.set(saveData1).then(function (doc) {
+                localStorage.setItem('userName', userName.value);
+                location.assign('../login/login.html');
+                name.value = '';
+                userName.value = '';
+                email.value = '';
+                password.value = '';
+                phone.value = '';
+            });
         }
     });
-
 }
