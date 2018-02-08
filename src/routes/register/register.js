ad75f94ea4f1a5f132daa63cb4801678d6e6c5d7
Shifa Haider <shifa.haider065@gmail.com>
2018-02-08 14:36:05 +0500

    save register work

--- src/routes/register/register.js
+++ src/routes/register/register.js
@@ -1,3 +1,12 @@
-/**
- * Created by DELL on 2/8/2018.
- */
+var db = firebase.firestore();
+function saveData(){
+    var saveData1 = {
+        name: document.getElementById('name').value,
+        userName: document.getElementById('userName').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        phoneNumber: document.getElementById('phone').value
+    };
+    db.collection('users').doc(saveData1.userName).set(saveData1)
+    
+}
