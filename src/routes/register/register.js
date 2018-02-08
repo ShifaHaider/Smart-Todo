8fadb1831a68262604dd8770cae6bd4dd98c3b54
Shifa Haider <shifa.haider065@gmail.com>
2018-02-08 14:52:16 +0500

    half project completed

--- src/routes/register/register.js
+++ src/routes/register/register.js
@@ -1,12 +1,32 @@
 var db = firebase.firestore();
 function saveData(){
+    var name = document.getElementById('name');
+    var userName = document.getElementById('userName');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var phone = document.getElementById('phone');
     var saveData1 = {
         name: document.getElementById('name').value,
-        userName: document.getElementById('userName').value,
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value,
-        phoneNumber: document.getElementById('phone').value
+        userName: userName.value,
+        email: email.value,
+        password: password.value,
+        phoneNumber: phone.value
     };
-    db.collection('users').doc(saveData1.userName).set(saveData1)
-    
+
+    var ref = db.collection('users').doc(saveData1.userName);
+
+    ref.get().then(function(doc){
+        if(doc.exists){
+            alert('UserName is not available');
+        }
+        else{
+            ref.set(saveData1);
+            name.value = '';
+            userName.value = '';
+            email.value = '';
+            password.value = '';
+            phone.value = '';
+        }
+    });
+
 }
